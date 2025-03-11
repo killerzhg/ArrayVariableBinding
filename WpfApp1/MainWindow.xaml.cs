f4581136572440d59f2ebfc79954318f0ae9e595
killerzhg <36200758@qq.com>
2025-03-11 15:38:38 +0800

    添加项目文件。

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Variable variable = new Variable();
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = variable;
+            
+            timer.Interval = TimeSpan.FromSeconds(1); // 设置定时器间隔为1秒
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                variable.LogList.Add(new MessageModel() { Index =i, Message = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")});
+            }
+            
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            variable.UpdateTextArray(0, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+}
---
+++ WpfApp1/Variable.cs
@@ -0,0 +1,48 @@
+﻿using LuxAutoTest.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+     public class Variable : NotifyBase
+    {
+        public Variable() { }
+
+        public ObservableCollection<MessageModel> LogList { get; set; } = new ObservableCollection<MessageModel>();
+
+
+
+        static private string[] _textArray = new string[3];
+        public string[] TextArray
+        {
+            get { return _textArray; }
+            set
+            {
+                _textArray = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void UpdateTextArray(int index, string value)
+        {
+            if (index >= 0 && index < _textArray.Length)
+            {
+                _textArray[index] = value;
+                OnPropertyChanged(nameof(TextArray));
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+    }
+    public class MessageModel
+    {
+        public int Index { get; set; }
+        public string Message { get; set; }
+    }
+}
